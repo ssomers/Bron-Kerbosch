db06281e6947c9930095210be8b7e122bbaa4ea6
Stein Somers <git@steinsomers.be>
2018-10-28 20:11:15 +0100

    clean up code and invoke the better alternative in Ver6

--- README.md
+++ README.md
@@ -26,35 +26,35 @@ Compared to the original forked from, the code is:
 
 ## Results
 
-Seconds spent on a particular machine, in particular random graphs (but results seem consistent), averaged over 7 runs:
+Seconds spent on a particular machine, in particular random graphs generated with --seed 1423439066 (but results seem consistent), averaged over 7 runs:
 
-* random graph of order 26 (full mesh size 325):
+* random graphs of order 26 (full mesh size 325):
 <table>
 <tr><th>size</th><th>Ver1</th><th>Ver2</th><th>Ver3</th><th>Ver4</th><th>Ver5</th><th>Ver6</th>
-<tr><th>220</th><td>0.01</td><td>0.00</td><td>0.00</td><td>0.01</td><td>0.00</td><td>0.01</td></tr>
-<tr><th>240</th><td>0.04</td><td>0.01</td><td>0.00</td><td>0.01</td><td>0.00</td><td>0.01</td></tr>
-<tr><th>260</th><td>0.09</td><td>0.01</td><td>0.01</td><td>0.01</td><td>0.00</td><td>0.01</td></tr>
-<tr><th>280</th><td>0.46</td><td>0.00</td><td>0.01</td><td>0.01</td><td>0.00</td><td>0.01</td></tr>
-<tr><th>300</th><td>2.62</td><td>0.01</td><td>0.00</td><td>0.01</td><td>0.00</td><td>0.01</td></tr>
+<tr><th>220</th><td>0.01</td><td>0.00</td><td>0.00</td><td>0.01</td><td>0.00</td><td>0.00</td></tr>
+<tr><th>240</th><td>0.05</td><td>0.00</td><td>0.00</td><td>0.01</td><td>0.00</td><td>0.00</td></tr>
+<tr><th>260</th><td>0.06</td><td>0.01</td><td>0.00</td><td>0.02</td><td>0.00</td><td>0.00</td></tr>
+<tr><th>280</th><td>0.39</td><td>0.01</td><td>0.00</td><td>0.02</td><td>0.00</td><td>0.00</td></tr>
+<tr><th>300</th><td>3.37</td><td>0.01</td><td>0.00</td><td>0.01</td><td>0.00</td><td>0.00</td></tr>
 </table>
 
-* random graph of order 82 (full mesh size 3321):
+* random graphs of order 82 (full mesh size 3321):
 <table>
 <tr><th>size</th><th>Ver1</th><th>Ver2</th><th>Ver3</th><th>Ver4</th><th>Ver5</th><th>Ver6</th>
-<tr><th>1800</th><td>0.42</td><td>0.23</td><td>0.20</td><td>0.65</td><td>0.14</td><td>0.65</td></tr>
-<tr><th>2000</th><td>2.05</td><td>0.73</td><td>0.60</td><td>1.66</td><td>0.32</td><td> 1.70</td></tr>
-<tr><th>2200</th><td>7.19</td><td>2.36</td><td>2.05</td><td>4.80</td><td>0.87</td><td> 4.75</td></tr>
+<tr><th>1800</th><td>0.41</td><td>0.28</td><td>0.22</td><td>0.62</td><td>0.15</td><td>0.16</td></tr>
+<tr><th>2000</th><td>1.38</td><td>0.67</td><td>0.60</td><td>1.60</td><td>0.32</td><td>0.35</td></tr>
+<tr><th>2200</th><td>8.96</td><td>2.42</td><td>1.96</td><td>4.90</td><td>0.81</td><td>0.84</td></tr>
 </table>
 
-* random graph of order 10k:
+* random graphs of order 10k:
 <table>
 <tr><th>size</th><th>Ver1</th><th>Ver2</th><th>Ver3</th><th>Ver4</th><th>Ver5</th><th>Ver6</th>
-<tr><th>10k</th><td>0.05</td><td>0.07</td><td>3.78</td><td>0.15</td><td>0.08</td><td>0.18</td></tr>
-<tr><th>20k</th><td>0.09</td><td>0.11</td><td>4.87</td><td>0.20</td><td>0.15</td><td>0.25</td></tr>
-<tr><th>30k</th><td>0.13</td><td>0.16</td><td>5.23</td><td>0.25</td><td>0.21</td><td>0.31</td></tr>
-<tr><th>40k</th><td>0.17</td><td>0.21</td><td>5.15</td><td>0.30</td><td>0.27</td><td>0.36</td></tr>
-<tr><th>50k</th><td>0.22</td><td>0.25</td><td>5.24</td><td>0.36</td><td>0.34</td><td>0.42</td></tr>
-<tr><th>60k</th><td>0.26</td><td>0.31</td><td>5.26</td><td>0.42</td><td>0.40</td><td>0.48</td></tr>
+<tr><th>10k</th><td>0.05</td><td>0.07</td><td>3.77</td><td>0.15</td><td>0.08</td><td>0.11</td></tr>
+<tr><th>20k</th><td>0.09</td><td>0.11</td><td>4.87</td><td>0.20</td><td>0.15</td><td>0.18</td></tr>
+<tr><th>30k</th><td>0.13</td><td>0.16</td><td>5.11</td><td>0.25</td><td>0.21</td><td>0.24</td></tr>
+<tr><th>40k</th><td>0.17</td><td>0.21</td><td>5.15</td><td>0.30</td><td>0.26</td><td>0.31</td></tr>
+<tr><th>50k</th><td>0.21</td><td>0.25</td><td>5.22</td><td>0.35</td><td>0.33</td><td>0.38</td></tr>
+<tr><th>60k</th><td>0.26</td><td>0.30</td><td>5.26</td><td>0.41</td><td>0.40</td><td>0.46</td></tr>
 </table>
 
 
--- bronker_bosch3.py
+++ bronker_bosch3.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from bronker_bosch2 import bron_kerbosch2, bron_kerbosch4
+from bronker_bosch2 import bron_kerbosch2, bron_kerbosch5
 from graph import UndirectedGraph
 from reporter import Reporter
 from dataclasses import dataclass, field
@@ -52,11 +52,11 @@ def bron_kerbosch6(graph: UndirectedGraph, clique: List[int],
         reporter.record(clique)
         return
 
-    for v in degeneracy_order_newer(graph=graph, candidates=candidates):
+    for v in degeneracy_order_smart(graph=graph, candidates=candidates):
         neighbours = graph.adjacencies[v]
         assert neighbours
         candidates.remove(v)
-        bron_kerbosch4(
+        bron_kerbosch5(
             graph=graph,
             clique=clique + [v],
             candidates=candidates.intersection(neighbours),
@@ -65,27 +65,13 @@ def bron_kerbosch6(graph: UndirectedGraph, clique: List[int],
         excluded.add(v)
 
 
-def degeneracy_order_array(graph: UndirectedGraph, candidates):
-    order = graph.order
-    infinite = order * 2  # still >= order after decrementing in each iteration
-    degree_per_node = [infinite] * order
-    for node in candidates:
-        degree_per_node[node] = graph.degree(node)
-
-    for _ in range(len(candidates)):
-        i = min(zip(degree_per_node, range(order)))[1]
-        yield i
-        degree_per_node[i] = infinite
-        for v in graph.adjacencies[i]:
-            degree_per_node[v] -= 1
-
-
 @dataclass(order=True)
 class PrioritizedItem:
     priority: int
     node: int = field(compare=False)
 
 
+# poorly performing alternative to degeneracy_order_smart
 def degeneracy_order_queue(graph: UndirectedGraph, candidates: Set[int]):
     degree_by_node = {c: graph.degree(c) for c in candidates}
     q: Any = queue.PriorityQueue()
@@ -107,62 +93,24 @@ def degeneracy_order_queue(graph: UndirectedGraph, candidates: Set[int]):
                     q.put(PrioritizedItem(priority=p - 1, node=v))
 
 
-def pick_with_lowest_degree(degree_by_node, nodes_per_degree):
-    assert all(node in nodes_per_degree[degree]
-               for node, degree in degree_by_node.items())
-    for d in range(len(nodes_per_degree)):
-        while nodes_per_degree[d]:
-            v = nodes_per_degree[d].pop()
-            if v in degree_by_node:
-                return v
-            else:
-                continue  # was moved to lower degree
-
-
-def pick_with_lowest_degree_newer(degree_per_node, nodes_per_degree, infinite):
+def pick_with_lowest_degree(degree_per_node, nodes_per_degree, infinite):
     assert all(node in nodes_per_degree[degree]
                for node, degree in enumerate(degree_per_node)
                if degree != infinite)
-    d = 0
-    while True:
+    for d in range(len(nodes_per_degree)):
         while nodes_per_degree[d]:
             v = nodes_per_degree[d].pop()
             if degree_per_node[v] != infinite:
                 return v
             else:
                 continue  # was moved to lower degree
-        d += 1
 
 
-def degeneracy_order_new(graph: UndirectedGraph, candidates: Set[int]):
-    if not candidates:
-        return
-    degree_by_node = {c: graph.degree(c) for c in candidates}
-    max_degree = max(degree_by_node.values())
-    nodes_per_degree: List[List[int]] = [[]
-                                         for degree in range(max_degree + 1)]
-    for c, degree in degree_by_node.items():
-        assert degree > 0  # FYI, isolated nodes were excluded up front
-        nodes_per_degree[degree].append(c)
-
-    for _ in range(len(candidates)):
-        i = pick_with_lowest_degree(degree_by_node, nodes_per_degree)
-        yield i
-        del degree_by_node[i]
-        for v in graph.adjacencies[i]:
-            d = degree_by_node.get(v)
-            if d is not None:
-                degree_by_node[v] = d - 1
-                # move to lower degree, but no need to remove the original one
-                nodes_per_degree[d - 1].append(v)
-
-
-def degeneracy_order_newer(graph: UndirectedGraph, candidates: Set[int]):
+def degeneracy_order_smart(graph: UndirectedGraph, candidates: Set[int]):
     if not candidates:
         return
     order = graph.order
     infinite = order * 2  # still >= order after decrementing in each iteration
-    # degree_by_node = {node: len(NEIGHBORS[node]) for node in candidates}
     degree_per_node = [infinite] * order
     max_degree = 0
     for node in candidates:
@@ -177,7 +125,7 @@ def degeneracy_order_newer(graph: UndirectedGraph, candidates: Set[int]):
         nodes_per_degree[degree].append(node)
 
     for _ in range(len(candidates)):
-        i = pick_with_lowest_degree_newer(
+        i = pick_with_lowest_degree(
             degree_per_node=degree_per_node,
             nodes_per_degree=nodes_per_degree,
             infinite=infinite)
