092c7e68b4921147b92cc6284f73d11857371ec8
Stein Somers <git@steinsomers.be>
2019-01-03 00:34:56 +0100

    Made the iterator of Ver8 genuine in Rust too

--- rust/bron_kerbosch/src/bron_kerbosch8.rs
+++ rust/bron_kerbosch/src/bron_kerbosch8.rs
@@ -12,8 +12,11 @@ pub fn explore(graph: &UndirectedGraph, reporter: &mut Reporter) {
     reporter.inc_count();
     let mut candidates = graph.connected_nodes();
     let mut excluded = HashSet::new();
-    let ordered = degeneracy_order_smart(graph, &candidates);
-    for v in ordered {
+    debug_assert_eq!(
+        degeneracy_order_smart(graph, &candidates).collect::<HashSet<Vertex>>(),
+        candidates
+    );
+    for v in degeneracy_order_smart(graph, &candidates) {
         let neighbours = graph.adjacencies(v);
         debug_assert!(!neighbours.is_empty());
         candidates.remove(&v);
@@ -32,28 +35,61 @@ pub fn explore(graph: &UndirectedGraph, reporter: &mut Reporter) {
     }
 }
 
-fn pick_with_lowest_degree(
-    degree_per_node: &Vec<u32>,
-    nodes_per_degree: &mut Vec<Vec<u32>>,
+#[derive(Debug)]
+struct DegeneracyOrderIter<'a> {
+    graph: &'a UndirectedGraph,
     infinite: u32,
-) -> Vertex {
-    debug_assert!(degree_per_node
-        .iter()
-        .enumerate()
-        .all(|(v, &d)| d == infinite || nodes_per_degree[d as usize].contains(&(v as u32))));
-    for d in 0..nodes_per_degree.len() {
-        while let Some(v) = nodes_per_degree[d].pop() {
-            if degree_per_node[v as usize] != infinite {
-                return v;
+    degree_per_node: Vec<u32>,
+    nodes_per_degree: Vec<Vec<u32>>,
+    num_left: usize,
+}
+impl<'a> DegeneracyOrderIter<'a> {
+    fn pick_with_lowest_degree(&mut self) -> Vertex {
+        debug_assert!(self
+            .degree_per_node
+            .iter()
+            .enumerate()
+            .all(|(v, &d)| d == self.infinite
+                || self.nodes_per_degree[d as usize].contains(&(v as u32))));
+        for d in 0..self.nodes_per_degree.len() {
+            while let Some(v) = self.nodes_per_degree[d].pop() {
+                if self.degree_per_node[v as usize] != self.infinite {
+                    self.degree_per_node[v as usize] = self.infinite;
+                    return v;
+                }
+                // else was moved to lower degree
             }
-            // else was moved to lower degree
         }
+        panic!("Should have returned");
     }
-    panic!("Should have returned");
 }
 
-fn degeneracy_order_smart(graph: &UndirectedGraph, candidates: &HashSet<Vertex>) -> Vec<Vertex> {
-    let order: u32 = graph.order();
+impl<'a> Iterator for DegeneracyOrderIter<'a> {
+    type Item = Vertex;
+    fn next(&mut self) -> Option<Vertex> {
+        if self.num_left == 0 {
+            None
+        } else {
+            self.num_left -= 1;
+            let i = self.pick_with_lowest_degree();
+            for &v in self.graph.adjacencies(i) {
+                let d = self.degree_per_node[v as usize];
+                if d != self.infinite {
+                    self.degree_per_node[v as usize] = d - 1;
+                    // move to lower degree, but no need to remove the original one
+                    self.nodes_per_degree[(d - 1) as usize].push(v)
+                }
+            }
+            Some(i)
+        }
+    }
+}
+
+fn degeneracy_order_smart<'a>(
+    graph: &'a UndirectedGraph,
+    candidates: &HashSet<Vertex>,
+) -> DegeneracyOrderIter<'a> {
+    let order = graph.order();
     let infinite: u32 = order * 2; // still >= order after decrementing in each iteration
     let mut degree_per_node: Vec<u32> = vec![infinite; order as usize];
     let mut max_degree: u32 = 0;
@@ -70,24 +106,11 @@ fn degeneracy_order_smart(graph: &UndirectedGraph, candidates: &HashSet<Vertex>)
         let degree = graph.degree(node);
         nodes_per_degree[degree as usize].push(node);
     }
-
-    let mut ordered: Vec<Vertex> = Vec::with_capacity(candidates.len());
-    for _ in 0..candidates.len() {
-        let i = pick_with_lowest_degree(&degree_per_node, &mut nodes_per_degree, infinite);
-        degree_per_node[i as usize] = infinite;
-        ordered.push(i);
-        for &v in graph.adjacencies(i) {
-            let d = degree_per_node[v as usize];
-            if d != infinite {
-                degree_per_node[v as usize] = d - 1;
-                // move to lower degree, but no need to remove the original one
-                nodes_per_degree[(d - 1) as usize].push(v)
-            }
-        }
+    DegeneracyOrderIter {
+        graph,
+        infinite,
+        degree_per_node,
+        nodes_per_degree,
+        num_left: candidates.len(),
     }
-    debug_assert_eq!(
-        ordered.iter().cloned().collect::<HashSet<Vertex>>(),
-        *candidates
-    );
-    ordered
 }
