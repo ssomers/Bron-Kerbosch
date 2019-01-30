9b08be378a206e76a08612d397fc029e64d718aa
Stein Somers <git@steinsomers.be>
2019-01-30 13:35:21 +0100

    restructure Go tests

--- go/src/bron_kerbosch/bron_kerbosch_test.go
+++ go/src/bron_kerbosch/bron_kerbosch_test.go
@@ -1,14 +1,14 @@
 package bron_kerbosch
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 )
 
-func bron_kerbosch(t *testing.T, adjacencies []VertexSet, expected_cliques [][]Vertex) {
-	graph := newUndirectedGraph(adjacencies)
+func bron_kerbosch(graph *UndirectedGraph) [][]Vertex {
 	var reporter SimpleReporter
-	bron_kerbosch1(&graph, &reporter)
+	bron_kerbosch1(graph, &reporter)
 	var cliques [][]Vertex = reporter.cliques
 	for _, clique := range cliques {
 		sort.Slice(clique, func(l int, r int) bool {
@@ -21,27 +21,29 @@ func bron_kerbosch(t *testing.T, adjacencies []VertexSet, expected_cliques [][]V
 				return d < 0
 			}
 		}
-		t.Errorf("got two cliques (#%d of length %d and #%d of length %d) with the same vertices",
+		panic(fmt.Sprintf("got two cliques (#%d of length %d and #%d of length %d) with the same vertices",
 			l+1, len(cliques[l]),
-			r+1, len(cliques[r]))
+			r+1, len(cliques[r])))
 		return false
 	})
-	areEqual(t, cliques, expected_cliques)
+	return cliques
 }
 
-func areEqual(t *testing.T, left [][]Vertex, right [][]Vertex) {
-	if len(left) != len(right) {
-		t.Errorf("%d <> %d cliques", len(left), len(right))
+func bk(t *testing.T, adjacencies []VertexSet, expected_cliques [][]Vertex) {
+	graph := newUndirectedGraph(adjacencies)
+	obtained_cliques := bron_kerbosch(&graph)
+	if len(obtained_cliques) != len(expected_cliques) {
+		t.Errorf("%d <> %d cliques", len(obtained_cliques), len(expected_cliques))
 	}
-	for iclique, lclique := range left {
-		rclique := right[iclique]
-		if len(lclique) != len(rclique) {
-			t.Errorf("clique #%d: %d <> %d vertices", iclique+1, len(lclique), len(rclique))
+	for j, o := range obtained_cliques {
+		e := expected_cliques[j]
+		if len(o) != len(e) {
+			t.Errorf("clique #%d: %d <> %d vertices", j+1, len(o), len(e))
 		} else {
-			for i, l := range lclique {
-				r := rclique[i]
+			for i, l := range o {
+				r := e[i]
 				if l != r {
-					t.Errorf("clique #%d vertex #%d/%d: %d <> %d", iclique+1, len(lclique), i+1, l, r)
+					t.Errorf("clique #%d vertex #%d/%d: %d <> %d", j+1, len(o), i+1, l, r)
 				}
 			}
 		}
@@ -49,40 +51,54 @@ func areEqual(t *testing.T, left [][]Vertex, right [][]Vertex) {
 }
 
 func TestOrder0(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{}, [][]Vertex{})
+	bk(t, []VertexSet{}, [][]Vertex{})
 }
 
 func TestOrder1(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{VertexSet{}}, [][]Vertex{})
+	bk(t, []VertexSet{VertexSet{}}, [][]Vertex{})
 }
 
 func TestOrder2_isolated(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{VertexSet{}, VertexSet{}}, [][]Vertex{})
+	bk(t,
+		[]VertexSet{
+			VertexSet{},
+			VertexSet{}},
+		[][]Vertex{})
 }
 
 func TestOrder2_connected(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{VertexSet{1: struct{}{}}, VertexSet{0: struct{}{}}}, [][]Vertex{[]Vertex{0, 1}})
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}},
+			VertexSet{0: struct{}{}}},
+		[][]Vertex{
+			[]Vertex{0, 1}})
 }
 
 func TestOrder3_size_1(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}},
-		VertexSet{0: struct{}{}},
-		VertexSet{}},
-		[][]Vertex{[]Vertex{0, 1}})
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{},
-		VertexSet{2: struct{}{}},
-		VertexSet{1: struct{}{}}},
-		[][]Vertex{[]Vertex{1, 2}})
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}},
+			VertexSet{0: struct{}{}},
+			VertexSet{}},
+		[][]Vertex{
+			[]Vertex{0, 1}})
+	bk(t,
+		[]VertexSet{
+			VertexSet{},
+			VertexSet{2: struct{}{}},
+			VertexSet{1: struct{}{}}},
+		[][]Vertex{
+			[]Vertex{1, 2}})
 
 }
 
 func TestOrder3_size_2(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}},
-		VertexSet{0: struct{}{}, 2: struct{}{}},
-		VertexSet{1: struct{}{}}},
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}},
+			VertexSet{0: struct{}{}, 2: struct{}{}},
+			VertexSet{1: struct{}{}}},
 		[][]Vertex{
 			[]Vertex{0, 1},
 			[]Vertex{1, 2}})
@@ -90,20 +106,23 @@ func TestOrder3_size_2(t *testing.T) {
 }
 
 func TestOrder3_size_3(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}, 2: struct{}{}},
-		VertexSet{0: struct{}{}, 2: struct{}{}},
-		VertexSet{0: struct{}{}, 1: struct{}{}}},
-		[][]Vertex{[]Vertex{0, 1, 2}})
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}, 2: struct{}{}},
+			VertexSet{0: struct{}{}, 2: struct{}{}},
+			VertexSet{0: struct{}{}, 1: struct{}{}}},
+		[][]Vertex{
+			[]Vertex{0, 1, 2}})
 
 }
 
 func TestOrder4_size_2_isolated(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}, 2: struct{}{}},
-		VertexSet{0: struct{}{}},
-		VertexSet{0: struct{}{}},
-		VertexSet{}},
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}, 2: struct{}{}},
+			VertexSet{0: struct{}{}},
+			VertexSet{0: struct{}{}},
+			VertexSet{}},
 		[][]Vertex{
 			[]Vertex{0, 1},
 			[]Vertex{0, 2}})
@@ -111,11 +130,12 @@ func TestOrder4_size_2_isolated(t *testing.T) {
 }
 
 func TestOrder4_size_2_connected(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}},
-		VertexSet{0: struct{}{}},
-		VertexSet{3: struct{}{}},
-		VertexSet{2: struct{}{}}},
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}},
+			VertexSet{0: struct{}{}},
+			VertexSet{3: struct{}{}},
+			VertexSet{2: struct{}{}}},
 		[][]Vertex{
 			[]Vertex{0, 1},
 			[]Vertex{2, 3}})
@@ -123,22 +143,24 @@ func TestOrder4_size_2_connected(t *testing.T) {
 }
 
 func TestOrder4_size_4_p(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}},
-		VertexSet{0: struct{}{}, 2: struct{}{}, 3: struct{}{}},
-		VertexSet{1: struct{}{}, 3: struct{}{}},
-		VertexSet{1: struct{}{}, 2: struct{}{}}},
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}},
+			VertexSet{0: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			VertexSet{1: struct{}{}, 3: struct{}{}},
+			VertexSet{1: struct{}{}, 2: struct{}{}}},
 		[][]Vertex{
 			[]Vertex{0, 1},
 			[]Vertex{1, 2, 3}})
 }
 
 func TestOrder4_size_4_square(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}, 3: struct{}{}},
-		VertexSet{0: struct{}{}, 2: struct{}{}},
-		VertexSet{1: struct{}{}, 3: struct{}{}},
-		VertexSet{0: struct{}{}, 2: struct{}{}}},
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}, 3: struct{}{}},
+			VertexSet{0: struct{}{}, 2: struct{}{}},
+			VertexSet{1: struct{}{}, 3: struct{}{}},
+			VertexSet{0: struct{}{}, 2: struct{}{}}},
 		[][]Vertex{
 			[]Vertex{0, 1},
 			[]Vertex{0, 3},
@@ -149,11 +171,12 @@ func TestOrder4_size_4_square(t *testing.T) {
 }
 
 func TestOrder4_size_5(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
-		VertexSet{0: struct{}{}, 2: struct{}{}},
-		VertexSet{0: struct{}{}, 1: struct{}{}, 3: struct{}{}},
-		VertexSet{0: struct{}{}, 2: struct{}{}}},
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			VertexSet{0: struct{}{}, 2: struct{}{}},
+			VertexSet{0: struct{}{}, 1: struct{}{}, 3: struct{}{}},
+			VertexSet{0: struct{}{}, 2: struct{}{}}},
 		[][]Vertex{
 			[]Vertex{0, 1, 2},
 			[]Vertex{0, 2, 3}})
@@ -161,25 +184,28 @@ func TestOrder4_size_5(t *testing.T) {
 }
 
 func TestOrder4_size_6(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
-		VertexSet{0: struct{}{}, 2: struct{}{}, 3: struct{}{}},
-		VertexSet{0: struct{}{}, 1: struct{}{}, 3: struct{}{}},
-		VertexSet{0: struct{}{}, 1: struct{}{}, 2: struct{}{}}},
-		[][]Vertex{[]Vertex{0, 1, 2, 3}})
+	bk(t,
+		[]VertexSet{
+			VertexSet{1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			VertexSet{0: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			VertexSet{0: struct{}{}, 1: struct{}{}, 3: struct{}{}},
+			VertexSet{0: struct{}{}, 1: struct{}{}, 2: struct{}{}}},
+		[][]Vertex{
+			[]Vertex{0, 1, 2, 3}})
 
 }
 
 func TestSample(t *testing.T) {
-	bron_kerbosch(t, []VertexSet{
-		VertexSet{},
-		VertexSet{2: struct{}{}, 3: struct{}{}, 4: struct{}{}},
-		VertexSet{1: struct{}{}, 3: struct{}{}, 4: struct{}{}, 5: struct{}{}},
-		VertexSet{1: struct{}{}, 2: struct{}{}, 4: struct{}{}, 5: struct{}{}},
-		VertexSet{1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
-		VertexSet{2: struct{}{}, 3: struct{}{}, 6: struct{}{}, 7: struct{}{}},
-		VertexSet{5: struct{}{}, 7: struct{}{}},
-		VertexSet{5: struct{}{}, 6: struct{}{}}},
+	bk(t,
+		[]VertexSet{
+			VertexSet{},
+			VertexSet{2: struct{}{}, 3: struct{}{}, 4: struct{}{}},
+			VertexSet{1: struct{}{}, 3: struct{}{}, 4: struct{}{}, 5: struct{}{}},
+			VertexSet{1: struct{}{}, 2: struct{}{}, 4: struct{}{}, 5: struct{}{}},
+			VertexSet{1: struct{}{}, 2: struct{}{}, 3: struct{}{}},
+			VertexSet{2: struct{}{}, 3: struct{}{}, 6: struct{}{}, 7: struct{}{}},
+			VertexSet{5: struct{}{}, 7: struct{}{}},
+			VertexSet{5: struct{}{}, 6: struct{}{}}},
 		[][]Vertex{
 			[]Vertex{1, 2, 3, 4},
 			[]Vertex{2, 3, 5},
