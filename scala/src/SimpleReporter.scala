f4c5ba571caca87abecb0017c1b6cd6f64c5dd0d
Stein Somers <git@steinsomers.be>
2019-02-10 21:29:30 +0100

    Scala Ver1 tested

--- scala/src/SimpleReporter.scala
+++ scala/src/SimpleReporter.scala
@@ -1,10 +1,12 @@
 import base.Clique
+
 import scala.collection.mutable.ArrayBuffer
 
 class SimpleReporter extends Reporter {
   var cliques = ArrayBuffer[Clique]()
 
   override def record(clique: Clique): Unit = {
+    assert(clique.size > 1)
     cliques += clique
   }
 }
--- scala/tst/BronKerboschTest.scala
+++ scala/tst/BronKerboschTest.scala
@@ -1,31 +1,41 @@
 import base.{Clique, Vertex}
 
-import scala.collection.immutable.TreeSet
+import scala.collection.immutable.{SortedSet, TreeSet}
 
 class BronKerboschTest extends org.scalatest.FunSuite {
 
-    type OrderedClique = TreeSet[Vertex]
-    type OrderedCliques = TreeSet[OrderedClique]
-
-    def order_cliques(cliques: List[Clique]): OrderedCliques = {
-        val ord = new Ordering[OrderedClique] {
-            def compare(a: OrderedClique, b: OrderedClique): Int = {
-                a.zip(b).map { case (va, vb) => va - vb }.filter { diff => diff == 0 }.head
-            }
-        }
-        new OrderedCliques()(ord) ++ cliques.map(clique => new OrderedClique ++ clique.toSet).toSet
+  type OrderedClique = SortedSet[Vertex]
+  type OrderedCliques = SortedSet[OrderedClique]
+  implicit def CliqueOrdering = new Ordering[OrderedClique] {
+    def compare(a: OrderedClique, b: OrderedClique): Int = {
+      assert(a.size > 1)
+      assert(b.size > 1)
+      a.iterator.zip(b.iterator)
+          .find { case (ea, eb) => ea != eb }
+          .map { case (ea, eb) => ea - eb }
+          .getOrElse(a.size - b.size)
     }
+  }
 
-    def bron_kerbosch(graph: UndirectedGraph): OrderedCliques = {
-        /*
+  def bk(adjacencylist: List[List[Vertex]],
+         expected_cliques: List[Clique]): Unit = {
+    val adjacencies = adjacencylist.map { neighbours =>
+      neighbours.toSet
+    }
+    val graph = new SlimUndirectedGraph(adjacencies)
+    val current = bron_kerbosch(graph)
+    assert(current == order_cliques(expected_cliques))
+  }
+
+  def bron_kerbosch(graph: UndirectedGraph): OrderedCliques = {
+    /*
         let mut first: Option<OrderedCliques> = None;
         for func in FUNCS {
-        */
-
-        val reporter = new SimpleReporter
-        bron_kerbosch1.explore(graph, reporter)
-        val current = order_cliques(reporter.cliques.toList)
-        current /*
+     */
+    val reporter = new SimpleReporter
+    bron_kerbosch1.explore(graph, reporter)
+    val cliques = reporter.cliques.toList
+    order_cliques(cliques) /*
             if first.is_none() {
                 first = Some(current);
             } else {
@@ -33,327 +43,106 @@ class BronKerboschTest extends org.scalatest.FunSuite {
             }
         }
         first.unwrap()
-            */
-    }
+   */
+  }
 
-    def bk(adjacencylist: List[List[Vertex]], expected_cliques: List[Clique]): Unit = {
-        val adjacencies = adjacencylist.map { neighbours => neighbours.toSet }
-        val graph = new SlimUndirectedGraph(adjacencies)
-        val current = bron_kerbosch(graph)
-        assert(current == order_cliques(expected_cliques))
-    }
+  def order_cliques(cliques: List[Clique]): OrderedCliques = {
+    new TreeSet[OrderedClique] ++ cliques
+      .map(clique => new TreeSet[Vertex] ++ clique.toSet)
+      .toSet
+  }
 
-    test("order 0") {
-        bk(List(), List())
-    }
+  test("order 0") {
+    bk(List(), List())
+  }
 
-    /*
+  test("order_1") {
+    bk(List(List()), List())
+  }
 
-    #[test
-    ] fn bk_order_1 () {
-        bk(vec ![vec ![]
-        ], vec !
-        [] );
-    }
+  test("order_2_isolated") {
+    bk(List(List(), List()), List())
+  }
 
-    #[test
-    ] fn bk_order_2_isolated () {
-        bk(vec ![vec ![]
-        , vec !
-        []], vec !
-        [] );
-    }
+  test("order_2_connected") {
+    bk(List(List(1), List(0)), List(List(0, 1)))
+  }
 
-    #[test
-    ] fn bk_order_2_connected () {
-        bk(vec ![vec ![1]
-        , vec !
-        [0
-        ]], vec !
-        [vec !
-        [0
-                , 1
-        ]] );
-    }
+  test("order_3_size_1") {
+    bk(List(List(1), List(0), List()), List(List(0, 1)));
+    bk(List(List(), List(2), List(1)), List(List(1, 2)))
+  }
 
-    #[test
-    ] fn bk_order_3_size_1 () {
-        bk(vec ![vec ![1]
-        , vec !
-        [0
-        ], vec !
-        []], vec !
-        [vec !
-        [0
-                , 1
-        ]] );
-        bk(vec ![vec ![]
-        , vec !
-        [2
-        ], vec !
-        [1
-        ]], vec !
-        [vec !
-        [1
-                , 2
-        ]] );
-    }
+  test("order_3_size_2") {
+    bk(List(List(1), List(0, 2), List(1)), List(List(0, 1), List(1, 2)))
+  }
 
-    #[test
-    ] fn bk_order_3_size_2 () {
-        bk(vec ![vec ![1]
-        , vec !
-        [0
-                , 2
-        ], vec !
-        [1
-        ]], vec !
-        [vec !
-        [0
-                , 1
-        ], vec !
-        [1
-                , 2
-        ]], );
-    }
+  test("order_3_size_3") {
+    bk(List(List(1, 2), List(0, 2), List(0, 1)), List(List(0, 1, 2)))
+  }
 
-    #[test
-    ] fn bk_order_3_size_3 () {
-        bk(vec ![vec ![1, 2]
-        , vec !
-        [0
-                , 2
-        ], vec !
-        [0
-                , 1
-        ]], vec !
-        [vec !
-        [0
-                , 1
-                , 2
-        ]], );
-    }
+  test("order_4_size_2_isolated") {
+    bk(List(List(1, 2), List(0), List(0), List()), List(List(0, 1), List(0, 2)))
+  }
 
-    #[test
-    ] fn bk_order_4_size_2_isolated () {
-        bk(vec ![vec ![1, 2]
-        , vec !
-        [0
-        ], vec !
-        [0
-        ], vec !
-        []], vec !
-        [vec !
-        [0
-                , 1
-        ], vec !
-        [0
-                , 2
-        ]], );
-    }
+  test("order_4_size_2_connected") {
+    bk(List(List(1), List(0), List(3), List(2)), List(List(0, 1), List(2, 3)))
+  }
 
-    #[test
-    ] fn bk_order_4_size_2_connected () {
-        bk(vec ![vec ![1]
-        , vec !
-        [0
-        ], vec !
-        [3
-        ], vec !
-        [2
-        ]], vec !
-        [vec !
-        [0
-                , 1
-        ], vec !
-        [2
-                , 3
-        ]], );
-    }
+  test("order_4_size_4_p") {
+    bk(
+      List(List(1), List(0, 2, 3), List(1, 3), List(1, 2)),
+      List(List(0, 1), List(1, 2, 3))
+    )
+  }
 
-    #[test
-    ] fn bk_order_4_size_4_p () {
-        bk(vec ![vec ![1]
-        , vec !
-        [0
-                , 2
-                , 3
-        ], vec !
-        [1
-                , 3
-        ], vec !
-        [1
-                , 2
-        ]], vec !
-        [vec !
-        [0
-                , 1
-        ], vec !
-        [1
-                , 2
-                , 3
-        ]], );
-    }
+  test("order_4_size_4_square") {
+    bk(
+      List(List(1, 3), List(0, 2), List(1, 3), List(0, 2)),
+      List(List(0, 1), List(0, 3), List(1, 2), List(2, 3))
+    )
+  }
 
-    #[test
-    ] fn bk_order_4_size_4_square () {
-        bk(vec ![vec ![1, 3]
-        , vec !
-        [0
-                , 2
-        ], vec !
-        [1
-                , 3
-        ], vec !
-        [0
-                , 2
-        ]], vec !
-        [vec !
-        [0
-                , 1
-        ], vec !
-        [0
-                , 3
-        ], vec !
-        [1
-                , 2
-        ], vec !
-        [2
-                , 3
-        ]], );
-    }
+  test("order_4_size_5") {
+    bk(
+      List(List(1, 2, 3), List(0, 2), List(0, 1, 3), List(0, 2)),
+      List(List(0, 1, 2), List(0, 2, 3))
+    )
+  }
 
-    #[test
-    ] fn bk_order_4_size_5 () {
-        bk(vec ![vec ![1, 2, 3]
-        , vec !
-        [0
-                , 2
-        ], vec !
-        [0
-                , 1
-                , 3
-        ], vec !
-        [0
-                , 2
-        ]], vec !
-        [vec !
-        [0
-                , 1
-                , 2
-        ], vec !
-        [0
-                , 2
-                , 3
-        ]], );
-    }
+  test("order_4_size_6") {
+    bk(
+      List(List(1, 2, 3), List(0, 2, 3), List(0, 1, 3), List(0, 1, 2)),
+      List(List(0, 1, 2, 3))
+    )
+  }
 
-    #[test
-    ] fn bk_order_4_size_6 () {
-        bk(vec ![vec ![1, 2, 3]
-        , vec !
-        [0
-                , 2
-                , 3
-        ], vec !
-        [0
-                , 1
-                , 3
-        ], vec !
-        [0
-                , 1
-                , 2
-        ]], vec !
-        [vec !
-        [0
-                , 1
-                , 2
-                , 3
-        ]], );
-    }
+  test("order_5_penultimate") {
+    bk(
+      List(
+        List(1, 2, 3, 4),
+        List(0, 2, 3, 4),
+        List(0, 1, 3, 4),
+        List(0, 1, 2),
+        List(0, 1, 2)
+      ),
+      List(List(0, 1, 2, 3), List(0, 1, 2, 4))
+    )
+  }
 
-    #[test
-    ] fn bk_order_5_penultimate () {
-        bk(vec ![vec ![1, 2, 3, 4]
-        , vec !
-        [0
-                , 2
-                , 3
-                , 4
-        ], vec !
-        [0
-                , 1
-                , 3
-                , 4
-        ], vec !
-        [0
-                , 1
-                , 2
-        ], vec !
-        [0
-                , 1
-                , 2
-        ], ], vec !
-        [vec !
-        [0
-                , 1
-                , 2
-                , 3
-        ], vec !
-        [0
-                , 1
-                , 2
-                , 4
-        ]], );
-    }
-
-    #[test
-    ] fn bk_sample () {
-        bk(vec ![vec ![]
-        , vec !
-        [2
-                , 3
-                , 4
-        ], vec !
-        [1
-                , 3
-                , 4
-                , 5
-        ], vec !
-        [1
-                , 2
-                , 4
-                , 5
-        ], vec !
-        [1
-                , 2
-                , 3
-        ], vec !
-        [2
-                , 3
-                , 6
-                , 7
-        ], vec !
-        [5
-                , 7
-        ], vec !
-        [5
-                , 6
-        ], ], vec !
-        [vec !
-        [1
-                , 2
-                , 3
-                , 4
-        ], vec !
-        [2
-                , 3
-                , 5
-        ], vec !
-        [5
-                , 6
-                , 7
-        ]], );
-    }
-    */
-};
+  test("sample") {
+    bk(
+      List(
+        List(),
+        List(2, 3, 4),
+        List(1, 3, 4, 5),
+        List(1, 2, 4, 5),
+        List(1, 2, 3),
+        List(2, 3, 6, 7),
+        List(5, 7),
+        List(5, 6)
+      ),
+      List(List(1, 2, 3, 4), List(2, 3, 5), List(5, 6, 7))
+    )
+  }
+}
