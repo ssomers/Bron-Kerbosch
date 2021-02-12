00a5588da14dffcf931c3f40ebd71df2ef54072d
Stein Somers <git@steinsomers.be>
2021-02-12 17:32:13 +0100

    C++: start integrating cppcoro

--- cpp17/BronKerbosch/BronKerbosch3MT.h
+++ cpp17/BronKerbosch/BronKerbosch3MT.h
@@ -7,10 +7,17 @@
 #include "GraphDegeneracy.h"
 #include "Reporter.h"
 #pragma warning(push)
+#pragma warning(disable: 4623)
+#include "cppcoro/async_generator.hpp"
+#include "cppcoro/sync_wait.hpp"
+#include "cppcoro/task.hpp"
+#pragma warning(pop)
+#pragma warning(push)
 #pragma warning(disable: 5220)
 #include <boost/fiber/buffered_channel.hpp>
 #pragma warning(pop)
 #include <thread>
+
 
 namespace BronKerbosch {
     template <typename VertexSet>
@@ -32,7 +39,6 @@ namespace BronKerbosch {
             }
         };
 
-        using start_channel_t = boost::fibers::buffered_channel<Vertex>;
         using visit_channel_t = boost::fibers::buffered_channel<VisitJob>;
         using report_channel_t = boost::fibers::buffered_channel<std::vector<Vertex>>;
 
@@ -56,68 +62,69 @@ namespace BronKerbosch {
         template <typename VertexSet, typename Reporter>
         static void explore(UndirectedGraph<VertexSet> const& graph, Reporter& reporter)
         {
-            start_channel_t start_channel{ 64 };
-            visit_channel_t visit_channel{ 64 };
-            report_channel_t report_channel{ 64 };
+            cppcoro::sync_wait([&]() -> cppcoro::task<>
+                {
+                    auto start_channel_gen = [&graph]() -> cppcoro::async_generator<Vertex>
+                    {
+                        auto ordering = DegeneracyOrderIter<VertexSet>::degeneracy_ordering(graph, -1);
+                        while (auto next = ordering.next()) {
+                            co_yield *next;
+                        }
+                    };
+                    auto start_channel = start_channel_gen();
 
-            std::vector<std::thread> threads;
-            threads.emplace_back([&start_channel, &graph]() {
-                auto ordering = DegeneracyOrderIter<VertexSet>::degeneracy_ordering(graph, -1);
-                while (auto next = ordering.next()) {
-                    start_channel.push(*next);
-                }
-                start_channel.close();
-                });
+                    visit_channel_t visit_channel{ 64 };
+                    report_channel_t report_channel{ 64 };
 
-            threads.emplace_back([&start_channel, &visit_channel, &graph]() {
-                auto excluded = Util::with_capacity<VertexSet>(std::max(1u, graph.order()) - 1);
-                for (Vertex v : start_channel) {
-                    auto const& neighbours = graph.neighbours(v);
-                    assert(!neighbours.empty());
-                    auto neighbouring_candidates = Util::difference(neighbours, excluded);
-                    if (neighbouring_candidates.empty()) {
-                        assert(!Util::are_disjoint(neighbours, excluded));
+                    auto excluded = Util::with_capacity<VertexSet>(std::max(1u, graph.order()) - 1);
+                    for (auto it = co_await start_channel.begin(); it != start_channel.end(); co_await ++it) {
+                        Vertex v = *it;
+                        auto const& neighbours = graph.neighbours(v);
+                        assert(!neighbours.empty());
+                        auto neighbouring_candidates = Util::difference(neighbours, excluded);
+                        if (neighbouring_candidates.empty()) {
+                            assert(!Util::are_disjoint(neighbours, excluded));
+                        }
+                        else {
+                            auto neighbouring_excluded = Util::intersection(neighbours, excluded);
+                            visit_channel.push(VisitJob{ v, std::move(neighbouring_candidates), std::move(neighbouring_excluded) });
+                        }
+                        excluded.insert(v);
                     }
-                    else {
-                        auto neighbouring_excluded = Util::intersection(neighbours, excluded);
-                        visit_channel.push(VisitJob{ v, std::move(neighbouring_candidates), std::move(neighbouring_excluded) });
+                    visit_channel.close();
+
+                    std::vector<std::thread> threads;
+                    std::atomic_int visitorsLive = NUM_VISITING_FIBERS;
+                    for (int i = 0; i < NUM_VISITING_FIBERS; ++i) {
+                        threads.emplace_back([&visit_channel, &report_channel, &visitorsLive, &graph]() {
+                            SendingReporter thread_reporter{ report_channel };
+                            for (VisitJob& job : visit_channel) {
+                                auto pile = VertexPile{ job.start };
+                                BronKerboschPivot::visit<VertexSet>(
+                                    graph,
+                                    thread_reporter,
+                                    PivotChoice::MaxDegreeLocal,
+                                    PivotChoice::MaxDegreeLocal,
+                                    std::move(job.candidates),
+                                    std::move(job.excluded),
+                                    &pile);
+                            }
+                            if (visitorsLive.fetch_sub(1) == 1)
+                                report_channel.close();
+                            });
                     }
-                    excluded.insert(v);
-                }
-                visit_channel.close();
-                });
 
-            std::atomic_int visitorsLive = NUM_VISITING_FIBERS;
-            for (int i = 0; i < NUM_VISITING_FIBERS; ++i) {
-                threads.emplace_back([&visit_channel, &report_channel, &visitorsLive, &graph]() {
-                    SendingReporter thread_reporter{ report_channel };
-                    for (VisitJob& job : visit_channel) {
-                        auto pile = VertexPile{ job.start };
-                        BronKerboschPivot::visit<VertexSet>(
-                            graph,
-                            thread_reporter,
-                            PivotChoice::MaxDegreeLocal,
-                            PivotChoice::MaxDegreeLocal,
-                            std::move(job.candidates),
-                            std::move(job.excluded),
-                            &pile);
+                    threads.emplace_back([&report_channel, &reporter]() {
+                        for (VertexList clique : report_channel) {
+                            reporter.record(std::move(clique));
+                        }
+                        });
+
+                    for (auto& thread : threads) {
+                        thread.join();
                     }
-                    if (visitorsLive.fetch_sub(1) == 1)
-                        report_channel.close();
-                    });
-            }
-
-            threads.emplace_back([&report_channel, &reporter]() {
-                for (auto clique : report_channel) {
-                    reporter.record(std::move(clique));
-                }
-                });
-
-            for (auto& thread : threads) {
-                thread.join();
-            }
-            assert(visitorsLive.load() == 0);
+                    assert(visitorsLive.load() == 0);
+                }());
         }
     };
-
 }
