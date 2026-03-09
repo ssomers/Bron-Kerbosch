9adc6af81efae086791c0df14a70a5b45ef85ea7
Stein Somers <git@steinsomers.be>
2026-03-09 17:13:23 +0100

    split up F# PriorityQueue

---
+++ fsharp/BronKerbosch/FortifiedCounter.fs
@@ -0,0 +1,51 @@
+namespace BronKerbosch
+
+open System.Diagnostics
+
+// Counts the coming and going of elements and, in debug build only, checks their identity.
+type FortifiedCounter<'T when 'T: equality> =
+    { mutable count: int
+#if DEBUG
+      mutable individuals: 'T list  // set-like, but don't force more traits on 'T here
+#endif
+    }
+
+    member private this.invariant: bool =
+#if DEBUG
+        this.count = this.individuals.Length
+#else
+        true
+#endif
+
+    member this.Contains(element: 'T) =
+#if DEBUG
+        this.individuals |> List.contains element
+#else
+        failwith "debug build only, please"
+#endif
+
+    member this.Add(element: 'T) =
+        assert this.invariant
+        this.count <- this.count + 1
+#if DEBUG
+        assert not (this.individuals |> List.contains element)
+        this.individuals <- element :: this.individuals
+#endif
+        assert this.invariant
+
+    member this.Remove(element: 'T) =
+        assert this.invariant
+        this.count <- this.count - 1
+#if DEBUG
+        assert (this.Contains element)
+        this.individuals <- this.individuals |> List.except [ element ]
+#endif
+        assert this.invariant
+
+module FortifiedCounter =
+    let create () : FortifiedCounter<'T> =
+        { count = 0
+#if DEBUG
+          individuals = []
+#endif
+        }
--- fsharp/BronKerbosch/PriorityQueue.fs
+++ fsharp/BronKerbosch/PriorityQueue.fs
@@ -4,68 +4,18 @@ open System.Diagnostics
 
 type Priority = int
 
-type PriorityQueue< ^T when ^T: comparison> =
-    { stackPerPriority: ^T list array
-      mutable dequeued: Set< ^T >
-      mutable numLeftToPick: int
-#if DEBUG
-      mutable leftToPick: Set< ^T >
-#endif
-    }
+type PriorityQueue< ^T> =
+    { stack_per_priority: ^T list array }
 
-    member this.Empty = this.numLeftToPick = 0
-
-    member this.Insert(element: ^T, priority: Priority) =
+    // If the same element is pushed again, its priority must be higher i.e. a lower value.
+    member this.Push(element: ^T, priority: Priority) =
         Debug.Assert(priority > 0)
-        Debug.Assert(this.dequeued.IsEmpty)
-#if DEBUG
-        Debug.Assert(not (this.leftToPick.Contains(element)))
-#endif
-        this.push (element, priority)
-        this.numLeftToPick <- this.numLeftToPick + 1
-#if DEBUG
-        this.leftToPick <- this.leftToPick.Add(element)
-        Debug.Assert(this.numLeftToPick = this.leftToPick.Count)
-#endif
+        this.stack_per_priority[priority - 1] <- element :: this.stack_per_priority[priority - 1]
 
-    // Requeue with a more urgent priority or dequeue.
-    member this.Promote(element: ^T, old_priority: Priority) : Priority =
-        Debug.Assert(this.numLeftToPick > 0)
-        Debug.Assert(old_priority > 0)
-        Debug.Assert(this.stackPerPriority[old_priority - 1] |> List.contains element)
-#if DEBUG
-        Debug.Assert(this.leftToPick.Contains(element))
-#endif
-        match old_priority - 1 with
-        | 0 ->
-            this.dequeue element
-            // Don't bother to find and remove the original entry in the queue,
-            // since the vertex will be skipped when popped, and thanks to
-            // numLeftToPick we might not need to pop it at all.
-            0
-        | new_priority ->
-            this.push (element, new_priority)
-            new_priority
-
-    member private this.push(element: ^T, priority: Priority) =
-        this.stackPerPriority[priority - 1] <- element :: this.stackPerPriority[priority - 1]
-
-    member this.Pop() : ^T =
-        Debug.Assert(this.numLeftToPick > 0)
-
-        let popped = this.raw_pop ()
-
-        if this.dequeued.Contains(popped) then
-            // this.Promote did not bother to clean up this one, so try the next one
-            this.Pop()
-        else
-            this.dequeue popped
-            popped
-
-
-    member private this.raw_pop() : ^T =
+    // May pop an element already popped earlier, in case it was pushed again.
+    member this.Pop() : ^T option =
         match
-            this.stackPerPriority
+            this.stack_per_priority
             |> Array.indexed
             |> Array.choose (fun (i: int, stack: ^T list) ->
                 match stack with
@@ -74,26 +24,10 @@ type PriorityQueue< ^T when ^T: comparison> =
             |> Array.tryHead
         with
         | Some(i, head, tail) ->
-            this.stackPerPriority[i] <- tail
-            head
-        | None -> failwith "Cannot pop more than has been put"
-
-    member private this.dequeue(element: ^T) =
-        Debug.Assert(this.numLeftToPick > 0)
-        this.numLeftToPick <- this.numLeftToPick - 1
-        this.dequeued <- this.dequeued.Add(element)
-#if DEBUG
-        Debug.Assert(this.leftToPick.Contains(element))
-        this.leftToPick <- this.leftToPick.Remove(element)
-        Debug.Assert(this.numLeftToPick = this.leftToPick.Count)
-#endif
+            this.stack_per_priority[i] <- tail
+            Some(head)
+        | None -> None
 
 module PriorityQueue =
-    let create<'T when ^T: comparison> (maxPriority: int) : PriorityQueue<'T> =
-        { stackPerPriority = Array.create maxPriority []
-          dequeued = Set.empty
-          numLeftToPick = 0
-#if DEBUG
-          leftToPick = Set.empty
-#endif
-        }
+    let create<'T> (maxPriority: int) : PriorityQueue<'T> =
+        { stack_per_priority = Array.create maxPriority [] }
