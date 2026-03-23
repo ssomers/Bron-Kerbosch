34e56b838d10a39d285dba39c3990b937a53069b
Stein Somers <git@steinsomers.be>
2026-03-23 16:10:31 +0100

    add priority_queue tests in Rust

--- rust/bron_kerbosch/src/core/priority_queue.rs
+++ rust/bron_kerbosch/src/core/priority_queue.rs
@@ -36,3 +36,42 @@ where
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_priority_queue_empty() {
+        let mut q: PriorityQueue<bool> = PriorityQueue::new(0);
+        assert!(q.pop().is_none());
+    }
+
+    #[test]
+    fn test_priority_queue_one() {
+        let mut q = PriorityQueue::new(1);
+        q.put(Priority::new(1).unwrap(), true);
+        assert_eq!(q.pop(), Some(true));
+        assert_eq!(q.pop(), None);
+    }
+
+    #[test]
+    fn test_priority_queue_two_down() {
+        let mut q = PriorityQueue::new(2);
+        q.put(Priority::new(2).unwrap(), 22);
+        q.put(Priority::new(1).unwrap(), 11);
+        assert_eq!(q.pop(), Some(11));
+        assert_eq!(q.pop(), Some(22));
+        assert_eq!(q.pop(), None);
+    }
+
+    #[test]
+    fn test_priority_queue_two_up() {
+        let mut q = PriorityQueue::new(2);
+        q.put(Priority::new(1).unwrap(), 22);
+        q.put(Priority::new(2).unwrap(), 11);
+        assert_eq!(q.pop(), Some(22));
+        assert_eq!(q.pop(), Some(11));
+        assert_eq!(q.pop(), None);
+    }
+}
