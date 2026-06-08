08758852a5ee5823aade67351d7399e500742462
Stein Somers <git@steinsomers.be>
2026-06-08 12:54:57 +0200

    got Expecto working - too much hassle

--- fsharp/BronKerbosch.Test/PriorityQueueTest.fs
+++ fsharp/BronKerbosch.Test/PriorityQueueTest.fs
@@ -1,34 +1,37 @@
 module BronKerbosch.Test.PriorityQueue
 
-open NUnit.Framework
 open BronKerbosch
+open Expecto
 
-[<Test>]
-let empty () =
-    let mutable q = PriorityQueue.init 0
-    Assert.That(PriorityQueue.Pop &q, Is.Null)
+let tests =
+    testList
+        "PriorityQueue"
+        [ test "empty" {
+              let mutable q = PriorityQueue.init 0
+              Expect.isNone (PriorityQueue.Pop &q) "empty"
+          }
 
-[<Test>]
-let one () =
-    let mutable q = PriorityQueue.init 1
-    PriorityQueue.Put(&q, 1, true)
-    Assert.That(PriorityQueue.Pop &q, Is.EqualTo(Some true))
-    Assert.That(PriorityQueue.Pop &q, Is.Null)
+          test "one" {
+              let mutable q = PriorityQueue.init 1
+              PriorityQueue.Put(&q, 1, true)
+              Expect.isSome (PriorityQueue.Pop &q) ""
+              Expect.isNone (PriorityQueue.Pop &q) "empty"
+          }
 
-[<Test>]
-let two_down () =
-    let mutable q = PriorityQueue.init 2
-    PriorityQueue.Put(&q, 2, 22)
-    PriorityQueue.Put(&q, 1, 11)
-    Assert.That(PriorityQueue.Pop &q, Is.EqualTo(Some 11))
-    Assert.That(PriorityQueue.Pop &q, Is.EqualTo(Some 22))
-    Assert.That(PriorityQueue.Pop &q, Is.Null)
+          test "two_down" {
+              let mutable q = PriorityQueue.init 2
+              PriorityQueue.Put(&q, 2, 22)
+              PriorityQueue.Put(&q, 1, 11)
+              Expect.equal (PriorityQueue.Pop &q) (Some 11) ""
+              Expect.equal (PriorityQueue.Pop &q) (Some 22) ""
+              Expect.isNone (PriorityQueue.Pop &q) "empty"
+          }
 
-[<Test>]
-let two_up () =
-    let mutable q = PriorityQueue.init 2
-    PriorityQueue.Put(&q, 1, 22)
-    PriorityQueue.Put(&q, 2, 11)
-    Assert.That(PriorityQueue.Pop &q, Is.EqualTo(Some 22))
-    Assert.That(PriorityQueue.Pop &q, Is.EqualTo(Some 11))
-    Assert.That(PriorityQueue.Pop &q, Is.Null)
+          test "two_up" {
+              let mutable q = PriorityQueue.init 2
+              PriorityQueue.Put(&q, 1, 22)
+              PriorityQueue.Put(&q, 2, 11)
+              Expect.equal (PriorityQueue.Pop &q) (Some 22) ""
+              Expect.equal (PriorityQueue.Pop &q) (Some 11) ""
+              Expect.isNone (PriorityQueue.Pop &q) "empty"
+          } ]
